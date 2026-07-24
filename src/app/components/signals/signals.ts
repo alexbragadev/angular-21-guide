449d661fce3b920ae0e4cd46a272b55ed3f98946
Alex A Braga <alexbraga107@gmail.com>
2026-07-24 13:04:03 -0300

    #Chore: add effect signal

--- src/app/components/signals/signals.ts
+++ src/app/components/signals/signals.ts
@@ -6,10 +6,19 @@ import { ButtonModule } from 'primeng/button';
 import { FormsModule } from '@angular/forms';
 import { InputTextModule } from 'primeng/inputtext';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { MessageModule } from 'primeng/message';
 
 @Component({
   selector: 'app-signals',
-  imports: [BreadcrumbModule, CommonModule, ButtonModule, FormsModule, InputTextModule, FloatLabelModule],
+  imports: [
+    BreadcrumbModule,
+    CommonModule,
+    ButtonModule,
+    FormsModule,
+    InputTextModule,
+    FloatLabelModule,
+    MessageModule,
+  ],
   templateUrl: './signals.html',
   styleUrl: './signals.css',
 })
@@ -19,18 +28,21 @@ export class SignalsComponent implements OnInit {
   count = this.state.count;
   name = '';
   country = '';
+  novaNota = 0;
 
   public items = [
     { label: 'Home', url: './', target: '_self' },
-    { label: 'Signals', url: './signals', target: '_self' }
+    { label: 'Signals', url: './signals', target: '_self' },
   ];
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   sendPersonalInfo() {
     this.state.setPersonName(this.name);
     this.state.setCountry(this.country);
+  }
+
+  adicionar() {
+    this.state.adicionarNota(this.novaNota);
   }
 }
--- src/app/services/counter-state.ts
+++ src/app/services/counter-state.ts
@@ -1,4 +1,4 @@
-import { computed, Injectable, signal } from '@angular/core';
+import { computed, effect, Injectable, signal } from '@angular/core';
 
 @Injectable({
   providedIn: 'root',
@@ -8,13 +8,39 @@ export class CounterStateService {
   private readonly _count = signal(0);
   private readonly _person_name = signal('Joseph');
   private readonly _country = signal('France');
+  private readonly _enviarAlerta = signal(false);
 
-  public person_information = computed(() => {return `Name: ${this._person_name()}, Country: ${this._country()}`});
+  notas = signal<number[]>([]);
 
   // public readonly
   readonly count = this._count.asReadonly();
   readonly person_name = this._person_name.asReadonly();
   readonly country = this._country.asReadonly();
+  readonly enviarAlerta = this._enviarAlerta.asReadonly();
+
+  person_information = computed(() => {return `Name: ${this._person_name()}, Country: ${this._country()}`});
+  media = computed(() => {
+    const notas = this.notas();
+    if (notas.length === 0) {
+      return 0;
+    }
+    const sum = notas.reduce((acc, nota) => acc + nota, 0);
+    return sum / notas.length;
+  });
+
+  constructor() {
+    effect(() => {
+      if(this.media() < 6){
+        this._enviarAlerta.update(() => true);
+      } else {
+        this._enviarAlerta.update(() => false);
+      }
+    });
+  }
+
+  adicionarNota(nota: number) {
+    this.notas.update(lista => [...lista, nota]);
+  }
 
   increment() {
     this._count.update((v) => v+1);
